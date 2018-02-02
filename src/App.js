5f19ba4278e1881b167014e56104cfd0a7eda34a
Ryan Sperzel <ryansperzel@gmail.com>
2018-02-02 16:56:55 -0500

    Landing page wired up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Route } from "react-router-dom"
+import Landing from './components/Landing'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Route exact path='/' render={props => <Landing {...props} />}/>
       </div>
     );
   }
