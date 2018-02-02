acc44261e82ef4a80849364badda443d5a88cc13
Ryan Sperzel <ryansperzel@gmail.com>
2018-02-02 17:30:02 -0500

    Added a broken FB login button to Landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,6 +3,11 @@ import React, { Component } from 'react'
 export default class Landing extends Component {
 
   render() {
-    return (<h1>This the Landing page</h1>)
+    return (
+      <div>
+        <h1>This the Landing page</h1>
+        <div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false"></div>
+      </div>
+    )
   }
 }
