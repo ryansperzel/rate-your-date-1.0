adf4ee0e2e078cd3a527f3c968129a04cdcc2947
Ryan Sperzel <ryansperzel@gmail.com>
2018-10-04 19:27:48 -0400

    Edited the ReadMe and the landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,7 +5,7 @@ export default class Landing extends Component {
   render() {
     return (
       <div>
-        <h1>This the Landing page</h1>
+        <h1>Changes to the landing page</h1>
         <div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false"></div>
       </div>
     )
